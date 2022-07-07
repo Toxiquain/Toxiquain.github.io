8482ec63fdb5d3c66fe8ed53e9dc11abbaafde7d
Ayoub Messaoudi <81723216+Toxiquain@users.noreply.github.com>
2022-07-07 02:25:25 +0100

    Add files via upload

---
+++ components/Accounts.php
@@ -0,0 +1,9 @@
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="icon-bar">
+                <a href="https://www.facebook.com/Toxiquain" class="facebook"><i class="fa fa-facebook"></i></a> 
+                <a href="https://twitter.com/amessaudi" class="twitter"><i class="fa fa-twitter"></i></a> 
+                <a href="https://www.instagram.com/uglycrush__/" class="google"><i class="fa fa-instagram"></i></a> 
+                <a href="https://www.linkedin.com/in/ayoub-messaoudi-16184020a/" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.youtube.com/channel/UCOmmJP7d5CVGIiHcu1RKmdg" class="youtube"><i class="fa fa-youtube"></i></a> 
+                </div>
---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="image/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:amessaudi@gmail.com">********@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="image/clock-icon.png" alt="">
+         <h3>Working</h3>
+         <p>24/24 | 7/7</p>
+      </div>
+
+      <div class="box">
+         <img src="image/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Casablanca, Maroc - 20000</a>
+      </div>
+
+      <div class="box">
+         <img src="image/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">**********</a>
+         <a href="tel:1112223333">**********</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. UglyDesign</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="image/loader.gif" alt="">
+</div>
